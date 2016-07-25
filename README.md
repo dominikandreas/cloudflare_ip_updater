d7d1269dbe6f7e98d2d2372d8e7271d35235bb7d
ddienlin <dominik@dienlin.net>
2016-07-25 08:28:52 +0200

    git history cleanup

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # cloudflare_ip_updater
+
 Updates the IPs for several DNS records on CloudFlare using their native API
+
+# Howto:
+* run ``python main.py``
+* it will ask you for the necessary info, such as email, api and subdomains
+* see if it works
+
+## Use crontab to run the updater every 5 minutes:
+* ``crontab -e``
+* add the following line: ``*/5 * * * * python /path_to_cloudflare_ip_updater/main.py``
+
---
+++ main.py
@@ -0,0 +1,83 @@
+import os,sys
+import json
+import requests
+from subprocess import Popen, PIPE
+import ConfigParser
+import urllib2
+import logging
+
+base_path = os.path.dirname(os.path.abspath(__file__))
+
+
+def parse_api_result(url_extension, headers, data=None):
+    base_api_url = "https://api.cloudflare.com/client/v4/zones"
+    if data is not None:
+        res = requests.put(base_api_url+url_extension, headers=headers, data=json.dumps(options)).json()
+    else:
+        res = requests.get(base_api_url+url_extension, headers=headers).json()
+    if not res.get("success", False):
+        print("error occurred for %s:\n%s\n"%(url_extension, res))
+    return res
+
+def get_options():
+    if not os.path.isfile(base_path+"/options.json"):
+        options = {entry: raw_input("please enter your %s\n"%entry) \
+            for entry in ["email", "api_key", "domain_name", "sub_domain_names"]}
+        options["sub_domain_names"] = [e.replace(",","").strip() \
+            for e in options["sub_domain_names"].split(" ") if "." in e]
+        with open(base_path+"/options.json","wb") as f:
+            f.write(json.dumps(options, sort_keys=True, indent=4))
+            return options
+    else:
+        with open(base_path+"/options.json", "r") as f:
+            return json.loads(f.read())
+
+def get_zone_id():
+    print("getting zone id")
+    return parse_api_result("?name="+domain_name, headers={"X-Auth-Email":email, "X-Auth-Key":api_key})["result"][0]["id"]
+
+def get_record_ids(zone_id, sub_domain_names):
+    print("getting record ids for %s"%sub_domain_names)
+    res = parse_api_result("/%s/dns_records"%(zone_id), headers={"X-Auth-Email":email, "X-Auth-Key":api_key})
+    return [(e["name"],e["id"], e["type"]) for e in res["result"] if (e["type"] == "A" or e["type"] == "AAAA") and e["name"] in sub_domain_names]
+
+def get_ip6():
+    # CAUTION! DIRTY INSUFFICIENTLY TESTED HACK INCOMING
+    def get_res(command):
+        return os.popen(command).read().split("\n")
+    ips = get_res("/sbin/ifconfig eth0 |  awk '/inet6/{print $3}'")
+    scopes = get_res("/sbin/ifconfig eth0 |  awk '/inet6/{print $4}'")
+    if len(sys.argv)>1 and sys.argv[1]=="debug":
+        print "ips, scopes:", ips, scopes
+    ip = [ip for ip,scope in zip(ips,scopes) if "Global" in scope][0]
+    if not ":" in ip:
+        raise RuntimeError("got invalid ipv6: %s"%ip)
+    return ip.split("/")[0]
+
+
+def get_ip():
+    return json.load(urllib2.urlopen('https://api.ipify.org/?format=json'))['ip']
+
+
+if __name__ == "__main__":
+    globals().update(get_options())
+    ipv4 = get_ip()
+    zone_id = get_zone_id()
+    record_ids = get_record_ids(zone_id, sub_domain_names)
+    try:
+        ipv6 = get_ip6()
+    except Exception as e:
+        logging.exception(e)
+        ipv6 = None
+        logging.warn("Unable to get IPv6, will ignore AAAA records")
+
+    for name, rec_id, rec_type in get_record_ids(zone_id, sub_domain_names):
+        if rec_type == "AAAA" and not ipv6 is None:
+            logging.warn("Skipping %s since its a AAAA record and no IPV6 available")
+            continue
+        print("updating ip for " + name)
+        options = {'id': rec_id, 'type': rec_type, 'name': name, 'content': ipv4 if rec_type=="A" else ipv6}
+        url = "https://api.cloudflare.com/client/v4/zones/%s/dns_records/%s"%(zone_id, rec_id)
+        headers={"X-Auth-Email":email, "X-Auth-Key":api_key}
+        response = requests.put(url, headers=headers, data=json.dumps(options)).json()
+        print("success: %s \n%s"%(response["success"],("" if response["success"] else response["errors"])))
